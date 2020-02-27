caf2ab0ed2e0b8fc83f9543e5a530f313bfaad60
marco <mbertacca>
2020-02-27 11:35:05 +0100

    v1.8

--- src/com/veryant/joe/Revision.java
+++ src/com/veryant/joe/Revision.java
@@ -20,7 +20,7 @@
 package com.veryant.joe;
 
 public class Revision {
-   public static final String id = "$Revision: v1.7.3-2-g60825dd$";
+   public static final String id = "$Revision: v1.8$";
    public static void main (String argv[]) {
       System.out.println (id);
    }
