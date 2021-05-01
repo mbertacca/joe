73382a7eb6391fcbb7cd13eeead78217dc67d885
mbertacca <marco.bertacca@gmail.com>
2021-05-01 17:09:26 +0200

    Changed the behavior of undeclared variable: accessing a never assigned variable now casts an exception instead of simply returning null. Note that could cause severe changes of behaviour

--- src/com/veryant/joe/Revision.java
+++ src/com/veryant/joe/Revision.java
@@ -20,7 +20,7 @@
 package com.veryant.joe;
 
 public class Revision {
-   public static final String id = "$Revision: v1.10$";
+   public static final String id = "$Revision: v1.11-3-g7e451dc$";
    public static void main (String argv[]) {
       System.out.println (id);
    }
