3eebe11e16d78e0e4a8d5f8edd8f6741ea94e22b
mbertacca <marco.bertacca@gmail.com>
2023-05-01 11:09:45 +0200

    Samples fixed for the new variables management,  v1.13 last version with old variable management

--- samples/MyCommand.java
+++ samples/MyCommand.java
@@ -1,10 +1,18 @@
-import java.util.Date;
+import com.veryant.joe.*;
 
-public class MyCommand extends com.veryant.joe.cs.CobolCommand {
-   public Date getDate() {
-      return new Date();
-   }
-   public double testnum(double a) {
-      return a;
+public class MyCommand {
+   public Object $for (Block init,
+                       Block cond,
+                       Block incr,
+                       Block code) throws Exception {
+      Object Return = null;
+      init.exec();
+      while ((Return=cond.exec()) != null &&
+              Return instanceof WBoolean &&
+              ((WBoolean) Return).booleanValue()) {
+         Return = code.exec();
+         incr.exec();
+      }
+      return Return;
    }
 }
