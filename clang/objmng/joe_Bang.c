6f7c6c5c8402f44deffe5d03e487c4788cdaa9fe
mbertacca <marco.bertacca@gmail.com>
2024-11-09 18:52:17 +0100

    fix on !raedLine (native)

--- clang/objmng/joe_Bang.c
+++ clang/objmng/joe_Bang.c
@@ -328,7 +328,7 @@ version (joe_Object self, int argc, joe_Object *argv, joe_Object *retval)
 {
    joe_StringBuilder msg = 0;
    joe_Object_assign (&msg, joe_StringBuilder_New ());
-   joe_StringBuilder_appendCharStar (msg, "JOE (native) Revision 1.32 ");
+   joe_StringBuilder_appendCharStar (msg, "JOE (native) Revision 1.33 ");
    joe_StringBuilder_appendCharStar (msg, __DATE__);
 #ifdef WIN32
    joe_StringBuilder_appendCharStar (msg, " Windows");
@@ -497,9 +497,13 @@ readLine (joe_Object self, int argc, joe_Object *argv, joe_Object *retval)
 {
    int i = joe_Files_getline (&inbuff, &inbufflen, stdin);
 
-   for (i--; i >= 0 && inbuff[i] < ' '; i--)
-     inbuff[i] = 0;
-   joe_Object_assign(retval, joe_String_New (inbuff));
+   if (i < 0) {
+      joe_Object_assign(retval, joe_Null_value);
+   } else {
+     for (i--; i >= 0 && inbuff[i] < ' '; i--)
+       inbuff[i] = 0;
+     joe_Object_assign(retval, joe_String_New (inbuff));
+   }
    return JOE_SUCCESS;
 }
 
